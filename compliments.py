59dc23e79f5f00c709f6a04fb7c374821306edfd
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2017-07-28 12:26:19 +0200

    Adjust indentation

--- compliments.py
+++ compliments.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 mail=Mail(app)
 
 app.config.update(
-        DEBUG=True,
-        #EMAIL SETTINGS
-        MAIL_SERVER='sslout.df.eu',
-        MAIL_PORT=465,
-        MAIL_USE_SSL=True,
-        MAIL_USERNAME = 'compliments@wikimedia.de',
-        MAIL_PASSWORD = config.password
-        )
+    DEBUG=True,
+    #EMAIL SETTINGS
+    MAIL_SERVER='sslout.df.eu',
+    MAIL_PORT=465,
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME = 'compliments@wikimedia.de',
+    MAIL_PASSWORD = config.password
+)
 
 mail=Mail(app)
 #addresses = ['jens.ohlig@wikimedia.de', 'charlie.kritschmar@wikimedia.de']
@@ -26,16 +26,16 @@ def compliment():
     # Special thanks to fundraising
     date = (time.strftime("%d/%m/%Y"))
     if (date == '12/01/2017'): 
-	addresses = ['till.mletzko@wikimedia.de', 'tobias.schumann@wikimedia.de', 'carsten.direske@wikimedia.de', 'wladimir.raizberg@wikimedia.de', 'mia.buller@wikimedia.de', 'hannah.weber@wikimedia.de', 'solveigh.mertins@wikimedia.de']
+        addresses = ['till.mletzko@wikimedia.de', 'tobias.schumann@wikimedia.de', 'carsten.direske@wikimedia.de', 'wladimir.raizberg@wikimedia.de', 'mia.buller@wikimedia.de', 'hannah.weber@wikimedia.de', 'solveigh.mertins@wikimedia.de']
 
     address = [random.choice(addresses)]
     with open('compliments.json') as json_data:
-            compliments = json.load(json_data)
+        compliments = json.load(json_data)
 
     msg = Message(
-          'Someone pressed the compliment button! Here is a compliment for you!',
-          sender='compliments@wikimedia.de',
-          recipients = address
+        'Someone pressed the compliment button! Here is a compliment for you!',
+        sender='compliments@wikimedia.de',
+        recipients = address
     )
     msg.body = random.choice(compliments)
     mail.send(msg)
